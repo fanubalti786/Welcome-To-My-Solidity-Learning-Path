e74736047006248c364d2e98b8094ac0f236afc1
fanubalti786 <fanubalti786@gmail.com>
2025-07-30 17:43:45 +0500

    // Sample contract to learn Remix IDE and how ownership and ownership transfer works in Solidity.

---
+++ contracts/sample.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract ownable 
+{
+    address public owner;
+    constructor()
+    {
+        owner = msg.sender;
+    }
+
+
+    modifier onlyOwner()
+    {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+
+    function setOwner(address _newOwner) public onlyOwner
+    {
+        require(_newOwner != address(0), "invalid address");
+        owner = _newOwner;
+    }
+
+
+    function onlyOwnerCanAccess() public onlyOwner
+    {
+        // ex 
+    }
+
+
+    function anyOneCanAccess() public 
+    {
+        // ex
+    }
+}
