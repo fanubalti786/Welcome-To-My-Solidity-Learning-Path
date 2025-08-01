da6cd8a46fb2cc913eda3b7b0e9861be642b89d4
fanubalti786 <fanubalti786@gmail.com>
2025-08-02 01:16:03 +0500

    changing in dynamic and fixed array in funtion  and contract level

--- contracts/multiple-array-declaration.sol
+++ contracts/multiple-array-declaration.sol
@@ -3,29 +3,42 @@
 pragma solidity ^0.8.0;
 
 contract arrayDeclaration {
+    // fixed arrays
+    uint256[3] public fixedArray1 = [1, 2, 3];
+    uint256[5] public fixedArray2;
+    uint256[] public fixedArray3 = new uint256[](3);
 
-    uint[] public stateArray; // Yeh function ke bahar declared hoti hai
+    // dynamic arrays
+    uint256[] public dynamicArray1 = [1, 2, 3];
+    uint256[] public dynamicArray2;
 
-
-   function arrayTypesExample(uint len) public view {
-    uint[] memory newArr = new uint[](3);
-
-    // Len parameter se length define ho rahi hai (dynamic initialization)
-    // but its not a actuall dynamic 
-    uint[] memory temp = new uint[](len);
-
-
-
-     // 🔸 Local fixed array (stack level — mostly for small types like uint[3])
-    uint[3] memory fixedArray = [uint(1), 2, 3]; // Fixed-length memory array
+    function arrayDeclarationInFunction(uint256 len) public {
+        // fixed array
+        uint256[5] memory memoryArray1;
 
 
-    // 🔸 Storage reference (points to state array, allows direct modification)
-    uint[] storage refArray = stateArray; // State array ka reference
+        // ❌ Inline memory array initialization is NOT allowed like this
+        // ✅ You must assign values one by one in memory fixed arrays
+        // uint256[3] memory memoryArray2 = [1, 2, 3]; //❌ Invalid syntax in memory
 
-    // 🔸 Memory copy of state array (read-only, changes do not affect original)
-    uint[] memory copyArray = stateArray; // State array ka snapshot (clone)
 
-   
-}
+        uint256[] memory memoryArray3 = new uint256[](3);
+
+
+        // Len parameter se length define ho rahi hai (dynamic initialization)
+        // but its not a actuall dynamic
+        uint256[] memory temp = new uint256[](len);
+
+
+
+        // dynamic array in funcion is not allowed
+    }
+
+    function arrayTypesExample(uint256 len) public view {
+        // 🔸 Storage reference (points to state array, allows direct modification)
+        uint256[3] storage refArray = fixedArray1; // State array ka reference
+
+        // 🔸 Memory copy of state array (read-only, changes do not affect original)
+        uint256[3] memory copyArray = fixedArray1; // State array ka snapshot (clone)
+    }
 }
