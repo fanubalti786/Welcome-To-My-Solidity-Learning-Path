35550d601e71295527464a7069218afee7eacc05
fanubalti786 <fanubalti786@gmail.com>
2025-08-01 18:30:16 +0500

    continue anAdded examples of break and continue statements in Solidity loopsd break

--- contracts/17-continue-and-break.sol
+++ contracts/17-continue-and-break.sol
@@ -3,11 +3,14 @@
 pragma solidity ^0.8.0;
 
 contract Loops {
-    bool public isTrue = true;
-
     function forLoop() public pure returns (uint256) {
         uint256 count = 0;
         for (uint256 i = 0; i < 10; i += 2) {
+            // Skip the current iteration and continue to the next loop cycle
+
+            if (i == 8) {
+                continue;
+            }
             count += 5;
         }
 
@@ -20,6 +23,10 @@ contract Loops {
         uint256 j = 0;
 
         while (j < 10) {
+            if (j == 6) {
+                // Exit the loop immediately
+                break;
+            }
             count += 5;
             j += 2;
         }
@@ -27,33 +34,18 @@ contract Loops {
         return count;
     }
 
-
-
     function dowhileLoop() public pure returns (uint256) {
         uint256 count = 0;
-        uint k = 0;
+        uint256 k = 0;
 
-        do{
+        do {
             count += 5;
             k += 2;
+            // Exit the loop immediately
 
-        }while (k < 10);
-
+            break;
+        } while (k < 10);
 
         return count;
-    }
-
-    function dynamicwhile() public view returns(bool)
-    {
-        while(isTrue)
-        {
-            return isTrue;
-        }
-
-        return false;
-    }
-
-    function change() public {
-        isTrue = !isTrue;
     }
 }
