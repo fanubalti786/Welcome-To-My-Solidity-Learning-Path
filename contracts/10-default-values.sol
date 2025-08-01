3e71e976cc508d0c98a58ec952c275dc6501b12d
Irfan Haider <fanubalti786@gmail.com>
2025-08-01 16:13:30 +0500

    Create 10-default-values.sol

---
+++ contracts/10-default-values.sol
@@ -0,0 +1,12 @@
+// SPDX-Licence-Identifier: GPL-3.0;
+
+pragma solidity ^0.8.0;
+
+contract defaultValues
+{
+    uint public age;
+    string public name;
+    bool public isture;
+    address public addr;
+    bytes32 public hash;
+}
