3b58d424d5fda789e0df208c44dc287dd7ce6d74
fanubalti786 <fanubalti786@gmail.com>
2025-08-01 16:52:58 +0500

    use of constant with low gas

---
+++ contracts/12-use-of-const.sol
@@ -0,0 +1,10 @@
+// SPDX-Licence-Identifier: GPL-3.0;
+
+pragma solidity ^0.8.0;
+
+contract _constant
+{
+    address public constant owner1= 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //373 gase
+    address public owner2 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //2485 gase
+     
+}
