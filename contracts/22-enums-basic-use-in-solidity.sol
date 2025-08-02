2a4b95b9f8a97ff8c9533e23647465782cffce98
fanubalti786 <fanubalti786@gmail.com>
2025-08-02 15:47:14 +0500

    basic use of enums declaration set get update manually through user

---
+++ contracts/22-enums-basic-use-in-solidity.sol
@@ -0,0 +1,47 @@
+// SPCX-Licence-Identifier: GPL-3.0
+
+
+pragma solidity ^0.8.0;
+
+
+contract Enums 
+{
+    enum Status {
+        Pending,
+        Shipped,
+        Rejected,
+        Accepted,
+        Cancel
+    }
+
+    Status public status;
+
+    function getStatus() public view returns(Status)
+    {
+        return status;
+    }
+
+    // when we take input then we can store stuats no with Status data type 
+    function setStatus(Status _status) public 
+    {
+        status = _status;
+    }
+
+    // when we don't take input then we can store stuats with Status which is manually created
+    function reject() public 
+    {
+        status = Status.Rejected;
+    }
+
+
+    function cancel() public 
+    {
+        status = Status.Cancel;
+    }
+
+    // we can store enum with constant value
+    function withconstantchange() public 
+    {
+        status = Status(1);
+    }
+}
